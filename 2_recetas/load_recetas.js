c97734d4ac8ba51c6aa60d645e5bb2c93976a723
Alberto <apmsuperior@gmail.com>
2023-04-20 14:40:48 -0600

    Create csv and improve json files from recipes

--- 2_recetas/load_recetas.js
+++ 2_recetas/load_recetas.js
@@ -1,73 +1,95 @@
 const cheerio = require("cheerio");
-var request = require('request');
-var fs = require('fs');
+const fs = require('fs');
 const axios = require("axios");
 
-const url = "https://cocina-casera.com/mx/recetas-de-guisos-mexicanos/"
-const linksArray = [];
-const recipesArray = [];
+/**
+ * Obtiene el listado de recetas de la página web
+ */
+const url = "https://cocina-casera.com/mx/recetas-de-guisos-mexicanos/";
+const ingredientsJson = [];
+const ingredientsCsv = ['Receta,Personas,Cantidad,Unidad,Ingrediente'];
 
-async function getGenre(){
-    try{
-        const response = await axios.get(url);
-        const $=cheerio.load(response.data)
-        const links = $('figure.sf-entry-featured-media a').map((index, element) => {
-            return $(element).attr('href');
-          }).get();
-        return links
-          //console.log(links);
-
-    }
-    catch(error){
-        //console.error(error);
-    }
-}
-
-async function acces_receta(link_url){
-    try{
-
-        const response = await axios.get(link_url);
-        const $=cheerio.load(response.data)
-        const h1Text = $('h1.sf-entry-title[itemprop="headline"]').text();
-        //console.log(h1Text);
-        
-        const recetas = $('div.entry-content > ul > li');
-        const recipeArray = [];
-        recetas.each(function() {
-        //console.log($(this).text());
-        recipeArray.push($(this).text());
-        });
-
-        const recipeObj = {
-            title: h1Text,
-            ingredients: recipeArray,
-          };
-
-        recipesArray.push(recipeObj);
-    }
-    catch(error){
-        //c
-        console.error(error);
-    }
-}
-
-const res = async () => {
-    const links = await getGenre();
-    for (let i = 0; i < links.length; i++) {
-        console.log(links[i]);
-        await acces_receta(links[i]);
-      }
-    
-    const json = JSON.stringify(recipesArray);
-      fs.writeFile("recipes.json", json, "utf8", (err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log("JSON file created");
-        }
-      });
+async function getGenre() {
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    const links = $('figure.sf-entry-featured-media a')
+      .map((index, element) => $(element).attr('href'))
+      .get();
+    return links;
+  } catch (error) {
+    console.error(error);
   }
+}
 
-  module.exports = res()
+async function accessRecipe(linkUrl) {
+  try {
+    const response = await axios.get(linkUrl);
+    const $ = cheerio.load(response.data);
 
+    const recipeName = $('h1.sf-entry-title[itemprop="headline"]').text();
+    const peopleAmount = $('div.entry-content > h2:first-of-type')
+      .text()
+      .match(/\d+/); // Número de personas que rinde la receta
+    const ingredients = [];
 
+    $('div.entry-content > ul > li').each(function() {
+      let ingrediente = $(this).text();
+      // Caso especial, al gusto
+      if (ingrediente.includes('al gusto')) {
+        ingrediente = ingrediente.replace('al gusto', '1 gusto')
+      }
+      // Patron para separar la cantidad de ingrediente, unidad de medida y el nombre
+      const pattern = /^([\w\s]+) - ((\d+\/\d+|\d+\.\d+|\d+))(\s+(de\s+)?(\w+))?(\s+\-\s+(\w+))?$/
+      const match = ingrediente.match(pattern);
+      if (match) {
+        const name = match[1].trim(); // Extrae el nombre del ingrediente
+        const quantity = match[2]; // Extrae la cantidad y conviértela en número
+        const unit = match[6]; // Extrae la unidad de medida
+        ingredients.push({
+          "Nombre": name,
+          "Cantidad": quantity,
+          "Unidad": unit
+        }); // Agrega el ingrediente al arreglo
+        ingredientsCsv.push(`${recipeName},${peopleAmount},${quantity},${unit},${name}`); // Agrega el ingrediente al arreglo
+      }
+    });
+    // Crea un objeto con la información de la receta para generar el json
+    const recipeObj = {
+      "Receta": recipeName,
+      "Personas": peopleAmount ? parseInt(peopleAmount[0]) : 0,
+      "Ingredientes": ingredients,
+    };
+
+    ingredientsJson.push(recipeObj);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function scrapeRecipes() {
+  const links = await getGenre();
+  for (let i = 0; i < links.length; i++) {
+    await accessRecipe(links[i]);
+  }
+  // Guarda el arreglo de recetas en un archivo CSV
+  fs.writeFile("recipes.csv", ingredientsCsv.join('\n'), "utf8", (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("CSV file created");
+    }
+  });
+
+  // Guarda el arreglo de recetas en un archivo JSON
+  const json = JSON.stringify(ingredientsJson, null, 2);
+  fs.writeFile("recipes.json", json, "utf8", (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("JSON file created");
+    }
+  });
+}
+
+module.exports = scrapeRecipes();
