0814ab82338c9984d7664cac89456b37d442e2e2
jmgutierreze <jmgutierreze@bch.bancodechile.cl>
2023-04-19 23:15:03 -0500

    Se agrego las modificaciones al etl2

--- 4_etl/etl2.py
+++ 4_etl/etl2.py
@@ -17,7 +17,32 @@ df = spark.read.option("multiline",True) \
         .option("encoding", "ISO-8859-1") \
         .json(file_path)
 
+df = df.select(
+    trim(col("Products")).alias("Products"),
+    trim(col("Autoservicio_bajo")).alias("Autoservicio_bajo"),
+    trim(col("Autoservicio_alto")).alias("Autoservicio_alto"),
+    trim(col("Mercado_ruedas_bajo")).alias("Mercado_ruedas_bajo"),
+    trim(col("Mercado_ruedas_alto")).alias("Mercado_ruedas_alto"),
+    trim(col("Mercado_publico_bajo")).alias("Mercado_publico_bajo"),
+    trim(col("Mercado_publico_alto")).alias("Mercado_publico_alto"),
+    trim(col("Ceda_bajo")).alias("Ceda_bajo"),
+    trim(col("Ceda_alto")).alias("Ceda_alto"),
+    trim(col("Medida")).alias("Medida")
+)
 
+df = df.select(
+    trim(col("Products")).alias("Products"),
+    split(df["Autoservicio_bajo"], "\\ ")[0].alias("Autoservicio_bajo"),
+    split(df["Autoservicio_alto"], "\\ ")[0].alias("Autoservicio_alto"),
+    split(df["Mercado_ruedas_bajo"], "\\ ")[0].alias("Mercado_ruedas_bajo"),
+    split(df["Mercado_ruedas_alto"], "\\ ")[0].alias("Mercado_ruedas_alto"),
+    split(df["Mercado_publico_bajo"], "\\ ")[0].alias("Mercado_publico_bajo"),
+    split(df["Mercado_publico_alto"], "\\ ")[0].alias("Mercado_publico_alto"),
+    split(df["Ceda_bajo"], "\\ ")[0].alias("Ceda_bajo"),
+    split(df["Ceda_alto"], "\\ ")[0].alias("Ceda_alto"),
+    trim(col("Medida")).alias("Medida")
+).filter(col("Products") != "Canales de Abasto") \
+.filter(col("Products") != "")
 
 
 df.printSchema()
