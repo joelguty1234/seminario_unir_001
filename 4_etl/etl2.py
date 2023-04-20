7aea4d22578903b63ae902b9a5f80707ae709d49
jmgutierreze <jmgutierreze@bch.bancodechile.cl>
2023-04-19 23:04:48 -0500

    Aggregate script refactoring json file of the central de abasto

--- 4_etl/etl2.py
+++ 4_etl/etl2.py
@@ -1,5 +1,7 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import split,col,explode,trim,regexp_replace,when
+from pyspark.sql.functions import split,col,explode,trim,regexp_replace,when,from_json
+from pyspark.sql.types import ArrayType, StructField, StringType, StructType
+
 # create a SparkSession
 spark = SparkSession.builder \
     .appName("Read JSON from GCS") \
@@ -9,15 +11,16 @@ spark = SparkSession.builder \
     .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", "./credenciales/keyfile.json") \
     .getOrCreate()
 
-file_path = "gs://test_yaku_001/seminario_001/16_de_Abril_de_2023.json"
+file_path = "gs://test_yaku_001/seminario_001/final_productos.json"
 
 df = spark.read.option("multiline",True) \
         .option("encoding", "ISO-8859-1") \
         .json(file_path)
 
 
-# show the first 20 rows of the DataFrame
+
+
 df.printSchema()
-df.show(20)
+df.show()
 
 spark.stop()
